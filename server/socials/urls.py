42d0016d1f0afc379c202ef7bde1de6966e0ecd3
lewisggp <lewisggp@users.noreply.github.com>
2024-09-27 18:41:28 -0400

    add social connect view endpoints

--- server/socials/urls.py
+++ server/socials/urls.py
@@ -1,16 +1,21 @@
 from django.urls import path
 from .views import (
-    GoogleLogin,
-    FacebookLogin,
-    TwitterV1Login,
-    TwitterLogin,
-    GitHubLogin
+    GoogleLogin, GoogleConnect,
+    FacebookLogin, FacebookConnect,
+    TwitterV1Login, TwitterV1Connect,
+    TwitterLogin, TwitterConnect,
+    GitHubLogin, GithubConnect,
 )
 
 urlpatterns = [
     path('google/', GoogleLogin.as_view(), name='google_login'),
+    path('google/connect/', GoogleConnect.as_view(), name='google_connect'),
     path('facebook/', FacebookLogin.as_view(), name='facebook_login'),
+    path('facebook/connect/', FacebookConnect.as_view(), name='facebook_connect'),
     path('twitter/v1/', TwitterV1Login.as_view(), name='twitter_v1_login'),
+    path('twitter/v1/connect/', TwitterV1Connect.as_view(), name='twitter_v1_connect'),
     path('twitter/', TwitterLogin.as_view(), name='twitter_login'),
+    path('twitter/connect/', TwitterConnect.as_view(), name='twitter_connect'),
     path('github/', GitHubLogin.as_view(), name='github_login'),
+    path('github/connect/', GithubConnect.as_view(), name='github_connect'),  
 ]
--- server/socials/views.py
+++ server/socials/views.py
@@ -5,8 +5,8 @@ from allauth.socialaccount.providers.twitter.views import TwitterOAuthAdapter
 from allauth.socialaccount.providers.twitter_oauth2.views import TwitterOAuth2Adapter
 from allauth.socialaccount.providers.github.views import GitHubOAuth2Adapter
 from allauth.socialaccount.providers.oauth2.client import OAuth2Client
-from dj_rest_auth.registration.views import SocialLoginView
-from dj_rest_auth.social_serializers import TwitterLoginSerializer
+from dj_rest_auth.registration.views import SocialConnectView, SocialLoginView
+from dj_rest_auth.social_serializers import TwitterConnectSerializer, TwitterLoginSerializer
 
 class GoogleLogin(SocialLoginView):
     authentication_classes = []
@@ -14,18 +14,40 @@ class GoogleLogin(SocialLoginView):
     callback_url = settings.GOOGLE_CALLBACK_URL
     client_class = OAuth2Client
 
+class GoogleConnect(SocialConnectView):
+    authentication_classes = []
+    adapter_class = GoogleOAuth2Adapter
+    callback_url = settings.GOOGLE_CALLBACK_URL
+    client_class = OAuth2Client
+   
 class FacebookLogin(SocialLoginView):
     authentication_classes = []
     adapter_class = FacebookOAuth2Adapter
     callback_url = settings.FACEBOOK_CALLBACK_URL
 
+class FacebookConnect(SocialConnectView):
+    authentication_classes = []
+    adapter_class = FacebookOAuth2Adapter
+    callback_url = settings.FACEBOOK_CALLBACK_URL
+
 class TwitterV1Login(SocialLoginView):
-    authentication_classes = []  # Disable authentication
+    authentication_classes = []
     serializer_class = TwitterLoginSerializer
     adapter_class = TwitterOAuthAdapter
-    callback_url = settings.TWITTER_CALLBACK_URL  # Add this in your settings
+    callback_url = settings.TWITTER_CALLBACK_URL
+
+class TwitterV1Connect(SocialConnectView):
+    authentication_classes = []
+    serializer_class = TwitterConnectSerializer
+    adapter_class = TwitterOAuthAdapter
+    callback_url = settings.TWITTER_CALLBACK_URL
 
 class TwitterLogin(SocialLoginView):
+    authentication_classes = []
+    adapter_class = TwitterOAuth2Adapter
+    callback_url = settings.TWITTER_CALLBACK_URL
+
+class TwitterConnect(SocialConnectView):
     authentication_classes = []
     adapter_class = TwitterOAuth2Adapter
     callback_url = settings.TWITTER_CALLBACK_URL
@@ -34,3 +56,10 @@ class GitHubLogin(SocialLoginView):
     authentication_classes = []
     adapter_class = GitHubOAuth2Adapter
     callback_url = settings.GITHUB_CALLBACK_URL
+    client_class = OAuth2Client
+
+class GithubConnect(SocialConnectView):
+    authentication_classes = []
+    adapter_class = GitHubOAuth2Adapter
+    callback_url = settings.GITHUB_CALLBACK_URL
+    client_class = OAuth2Client
